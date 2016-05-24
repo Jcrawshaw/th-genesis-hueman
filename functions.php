51cd78217bf068ab178a2e3338e33e0aae2fe179
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2016-05-24 15:24:47 -0400

    Added home-top and home-bottom widget areas

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
@@ -15,3 +15,15 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Register widget areas
+genesis_register_sidebar( array(
+  'id'          => 'home-top',
+  'name'        => __( 'Home Top', 'treehouse-hueman-genesis' ),
+  'description' => __( 'Widgets in this section will display in the top widget area on the homepage.', 'treehouse-hueman-genesis' ),
+) );
+genesis_register_sidebar( array(
+  'id'          => 'home-bottom',
+  'name'        => __( 'Home Bottom', 'treehouse-hueman-genesis' ),
+  'description' => __( 'Widgets in this section will display in the bottom widget area on the homepage.', 'treehouse-hueman-genesis' ),
+) );
